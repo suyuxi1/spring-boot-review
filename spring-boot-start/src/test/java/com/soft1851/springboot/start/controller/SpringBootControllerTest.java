2216c5eb433fb47b9f80826a38b911ef129e8a13
su <2296887348@qq.com>
2020-05-12 15:52:12 +0800

    spring-boot-start模块学习

---
+++ spring-boot-start/src/test/java/com/soft1851/springboot/start/controller/SpringBootControllerTest.java
@@ -0,0 +1,39 @@
+package com.soft1851.springboot.start.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+
+@SpringBootTest
+class SpringBootControllerTest {
+
+    @Autowired
+    private SpringBootController springBootController;
+    // http请求的模拟对象，桩对象
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(springBootController).build();
+    }
+
+    @Test
+    void hello() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                //get请求方式
+                .get("/api/hello")
+                .accept(MediaType.APPLICATION_JSON))
+                //http返回状态码
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content()
+                        .string("Hello, Spring Boot!"));
+        System.out.println();
+    }
+}
